c58007728c9322fc9f5fe339db9d3002224928b6
Jeffrey Yang <snjyor@163.com>
2024-07-09 15:09:22 +0800

    Delete example directory

--- example/solders/pubkey/from_string.py
+++
@@ -1,9 +0,0 @@
-### solders.pubkey.Pubkey.from_string
-# 从地址字符串的获取公钥对象
-
-
-from solders.pubkey import Pubkey
-
-demo_pubkey = Pubkey.from_string("EBziZDuZwJNSMx57fZFqgzXVLssWZSLMQzweVkGzRdVv")
-print(demo_pubkey)  # EBziZDuZwJNSMx57fZFqgzXVLssWZSLMQzweVkGzRdVv type: Pubkey
-
